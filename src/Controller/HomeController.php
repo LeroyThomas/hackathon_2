8f887678926f93f8ccd656c6b21018c019ecafc7
Thomas Leroy <thomasleroy50@gmail.com>
2021-01-14 11:05:24 +0100

    ranQuestion page accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +11,21 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @return Response
      */
-    public function index(): Response
+    public function showQuestion(): Response
     {
+        $repository = $this->getDoctrine()
+            ->getRepository(Question::class);
+
+        $randQuestion = $repository->createQueryBuilder('q')
+                        ->orderBy('RAND()')
+                        ->setMaxResults(1)
+                        ->getQuery()
+                        ->execute();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'randQuestion'=>$randQuestion,
         ]);
     }
 }
