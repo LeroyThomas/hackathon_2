cff8be13822b7740d6af4255f5ed2360e112d76a
Thomas Leroy <thomasleroy50@gmail.com>
2021-01-14 12:11:25 +0100

    create form Question accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Question;
+use App\Form\QuestionType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +16,19 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      * @return Response
      */
-    public function showQuestion(): Response
+    public function showQuestion(Request $request): Response
     {
+        $question = new Question();
+        $form = $this->createForm(QuestionType::class, $question);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($question);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('home');
+        }
         $repository = $this->getDoctrine()
             ->getRepository(Question::class);
 
@@ -26,6 +40,8 @@ class HomeController extends AbstractController
 
         return $this->render('home/index.html.twig', [
             'randQuestion'=>$randQuestion,
+            'question'=>$question,
+            'form'=>$form->createView(),
         ]);
     }
 }
