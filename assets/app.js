5027821e0db101955a16071a31f64d0e31b3b3fc
Charly <charly.vial04@gmail.fr>
2021-01-14 11:14:22 +0100

    add event onClick on Timer

--- assets/app.js
+++ assets/app.js
@@ -14,26 +14,28 @@ const $ = require('jquery');
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
 
-// Timer Start
+// Timer start
 const startingMinutes = 1;
 let time = startingMinutes * 60;
 
 const countdownEl = document.getElementById('timer');
 
-let x = setInterval(updateCountdown, 1000);
+const buttons = document.getElementById('btn-start');
+buttons.addEventListener('click', function() {
+    let x = setInterval(updateCountdown, 1000);
 
-function updateCountdown() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
+    function updateCountdown() {
+        const minutes = Math.floor(time / 60);
+        let seconds = time % 60;
 
-    seconds = seconds < 10 ? '0' + seconds : seconds;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        countdownEl.innerHTML = `${minutes}: ${seconds}`;
+        time--;
 
-    countdownEl.innerHTML = `${minutes}: ${seconds}`;
-    time--;
-
-    if (minutes <= 0 && seconds <= 0) {
-        clearInterval(x);
-        document.getElementById("timer").innerHTML = "TO LATE !";
+        if (minutes <= 0 && seconds <= 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "TO LATE !";
+        }
     }
-}
-// Timer End
+});
+//Timer End
