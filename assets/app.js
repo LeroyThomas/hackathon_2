70798b1377189b0b272ec54e66d8d92fc164ca14
Charly <charly.vial04@gmail.fr>
2021-01-14 09:38:07 +0100

    add Timer

--- assets/app.js
+++ assets/app.js
@@ -14,3 +14,22 @@ const $ = require('jquery');
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
+
+// Timer Start
+const startingMinutes = 10;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById('timer');
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+}
+// Timer End
