a38e2b77d50aa5a5c18b153d4282af7a7f57752b
Thomas Leroy <thomasleroy50@gmail.com>
2021-01-15 12:50:11 +0100

    image ok bof

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,15 @@ Encore
     .setOutputPath('public/build/')
     .copyFiles({from: './assets/sounds',
     to: 'sounds/[path][name].[ext]',
+
     // if versioning is enabled, add the file hash too
     //to: 'images/[path][name].[hash:8].[ext]',
     // only copy files matching this pattern
     //pattern: /\.(png|jpg|jpeg)$/
     })
+    // .copyFiles({
+    //     from: './assets/images',
+    // }
     // public path used by the web server to access the output path
     .setPublicPath('/build')
     // only needed for CDN's or sub-directory deploy
