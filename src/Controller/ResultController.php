ec6f1f4e7c49c756af3d02ab9af8ec944031136f
ClaireV38 <claireverdier00@gmail.com>
2021-01-15 10:15:21 +0100

    add winner display

--- src/Controller/ResultController.php
+++ src/Controller/ResultController.php
@@ -11,6 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use App\Entity\User;
 
 class ResultController extends AbstractController
 {
@@ -25,6 +26,18 @@ class ResultController extends AbstractController
         $results = $resultsGameRepository->findAll();
         return $this->render('Result/result.html.twig', [
             'results' => $results
+        ]);
+    }
+
+    /**
+     * @Route("/winner/{id}", name="winner", methods={"GET"})
+     * @param User $winner
+     * @return Response
+     */
+    public function winner(User $winner): Response
+    {
+        return $this->render('Result/winner.html.twig', [
+            'winner' => $winner
         ]);
     }
 }
